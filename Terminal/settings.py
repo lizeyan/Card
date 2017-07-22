a06e4db9ac5632dc7874879c122aff4193964dfa
lizeyan <zy-li14@hotmail.com>
2017-07-22 23:52:09 +0800

    encrypt

--- Terminal/settings.py
+++ Terminal/settings.py
@@ -6,3 +6,4 @@ FORMAT = '%(asctime)-15s %(message)s'
 logging.basicConfig(handlers=[logging.FileHandler("terminal.log", encoding="utf-8"), logging.StreamHandler(sys.stdout)], level=logging.DEBUG, format=FORMAT)
 HOST = "http://123.207.163.49:8000/"
 KEY = "akxcuO9.2nJe!\"1Do\'#uNa.0r#lb8Ia4%FX=z@hlvEi2et-*ssf62t#047YO@@(qQ.!HEZ--~LiNSpSvhqeGcq-q#UjaI*aj6J'Rpv^xnSejjX%ih2gUZ(0JVOYqOf!~"
+IV = "5413251766712441"
